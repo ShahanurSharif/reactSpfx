e27535baadb0fb98d1c11bfbd00c903c629b4f1a
sharif <shahanur700@gmail.com>
2025-05-23 03:33:14 +0800

    pnpconfig

--- src/pnpjsConfig.ts
+++ src/pnpjsConfig.ts
@@ -2,13 +2,15 @@ import {spfi, SPFI, SPFx} from "@pnp/sp"
 import {WebPartContext} from "@microsoft/sp-webpart-base";
 import {LogLevel, PnPLogging} from "@pnp/logging";
 
-var _sp: SPFI = undefined; 
+var _sp: SPFI = null;
 
-export const getSP = (context?: WebPartContext): SPFI | null => {
-    if (context!=null) {
+export const getSP = (context: WebPartContext): SPFI => {
+    if (_sp === null && context != null) {
         _sp = spfi()
             .using(SPFx(context))
-            .using(PnPLogging(LogLevel.Warning))
+            .using(
+                PnPLogging(LogLevel.Warning)
+            )
     }
     return _sp;
 }
